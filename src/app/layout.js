493b1f5dbf4be97751dc1256e910ab645ee92799
kiks277 <emp.saray@gmail.com>
2026-03-18 04:25:07 +0800

    v2.3.0 Added Insights and Analytics for Vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 import Footer from '@/components/layout/Footer'
 import CookieConsent from '@/components/ui/CookieConsent'
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata = {
   title: 'WishPanda — Create & Share Gift Wishlists',
   description: 'Create shareable gift wishlists for birthdays, weddings, and holidays. Your friends can view and mark gifts as chosen.',
